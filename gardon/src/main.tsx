24f4eb60113bb5a1edf58604ca8c1dc6e9c7e609
Meikelele <michal.bak2002@interia.pl>
2025-09-13 14:16:20 +0200

    config(): added Mantine (UI Library) & linked

--- gardon/src/main.tsx
+++ gardon/src/main.tsx
@@ -2,11 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 
 import './styles/globals.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        colors: {
+          brand: ['#e6f4ef', '#cce9df', '#99d3bf', '#66bd9f', '#33a77f', '#00915f', '#00734c', '#005539', '#003726', '#001913'],
+        },
+        primaryColor: 'brand',
+        defaultRadius: 'md',
+      }}>
+      <Notifications />
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
