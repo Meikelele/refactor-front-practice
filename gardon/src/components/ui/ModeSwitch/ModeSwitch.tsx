49dfe11ebbde483e6a5fc9526e2c72ae50acd99b
Meikelele <michal.bak2002@interia.pl>
2025-09-16 22:55:01 +0200

    feat(): created slider component with app logo for changing schedule and manual mode

--- gardon/src/components/ui/ModeSwitch/ModeSwitch.tsx
+++ gardon/src/components/ui/ModeSwitch/ModeSwitch.tsx
@@ -1,24 +1,34 @@
-// import type { ChangeEvent } from 'react';
+import './ModeSwitch.scss';
 
-// type Props = {
-//   checked: boolean;
-//   onChange: (checked: boolean) => void;
-//   ariaLabel?: string;
-// };
+type Props = {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
 
-export default function ModeSwitch() {
-  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   onChange(e.currentTarget.checked);
-  // };
+export default function ModeSwitch({checked, onChange}: Props) {
 
   return (
-    <label className="switchMode">
-      <input
-        type="checkbox"
-        // checked={checked}
-        // onChange={handleChange}
-      />
-      <span className="sliderMode" />
-    </label>
+    <button
+      type='button'
+      className='modeSwitch'
+      onClick={ () => onChange(!checked)}
+      aria-pressed={checked}
+    >
+      <span className='modeSwitch__thumb'
+        style={{
+            left: checked ? "40px" : "4px",
+            background: checked
+              ? "#33c476"
+              : "#c04dae"
+          }}
+      >
+        <img
+          src={checked ? "../../../../public/logoONswitch.png" : "../../../../public/logoOFFswitch.png"}
+          alt={checked ? "ON Logo" : "OFF Logo"}
+          style={{ width: 20, height: 20 }}
+        />
+
+      </span>
+    </button>
   );
 }
