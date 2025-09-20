e88c1857a3bcb58b746cd87777c49f9f76d6f729
Meikelele <michal.bak2002@interia.pl>
2025-09-20 23:38:37 +0200

    ref(Section): created Section, a template

--- gardon/src/components/ui/Section/Section.tsx
+++ gardon/src/components/ui/Section/Section.tsx
@@ -1,15 +1,21 @@
-import { Box } from "@mantine/core";
 import "./Section.scss";
 import type { ReactNode } from "react"; 
+import Divider from "../Divider/Divider";
 
 type Props = {
-    children?: ReactNode
+    children?: ReactNode,
+    title?: string
 }
 
-export default function Section({children}: Props) {
+export default function Section({children, title}: Props) {
     return(
-        <Box className="section">
+        <div className="section">
+            <div className="section__header">
+                <h1>{title ? title : '<your_title_here>'}</h1>
+            </div>
+            <Divider w={90} h={0.12} borderType="solid" color="gray"/>
+            <div style={{marginBottom: `0.8rem`}} />
             {children}
-        </Box>
+        </div>
     )    
 }
