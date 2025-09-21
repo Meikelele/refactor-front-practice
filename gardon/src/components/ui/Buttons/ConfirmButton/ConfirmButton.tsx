5220b0c042c21383855326aa0a34491a316549b6
Meikelele <michal.bak2002@interia.pl>
2025-09-21 21:42:42 +0200

    feat(ConfirmButton): created and stylized Confirm Button for Schedule Mode section

---
+++ gardon/src/components/ui/Buttons/ConfirmButton/ConfirmButton.tsx
@@ -0,0 +1,12 @@
+import './ConfirmButton.scss';
+
+export default function ConfirmButton() {
+
+    return (
+        <div className="confirmButton">
+            <button>
+                Confirm
+            </button>
+        </div>
+    );
+}
