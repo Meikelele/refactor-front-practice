7b89db4831a1b8e09474eebd343f51580c8ebaf0
Meikelele <michal.bak2002@interia.pl>
2025-09-22 18:28:40 +0200

    feat(): ModeSwitch reacts on state and shows/hides ManualSection and ScheduleSection

--- gardon/src/features/mode/ModeHeader.tsx
+++ gardon/src/features/mode/ModeHeader.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
 import ModeSwitch from "../../components/ui/Switches/ModeSwitch/ModeSwitch";
 import "./ModeHeader.scss"
 
-export default function ModeHeader() {  
-const [scheduleMode, setScheduleMode] = useState(true);
+type Props = {
+  isScheduleMode: boolean;
+  setIsScheduleMode: (isScheduleMode: boolean) => void;
+}
+
+export default function ModeHeader({isScheduleMode, setIsScheduleMode}: Props) {  
 
 return (
     <div className="modeHeader__container">
         <div>
             <div className="modeHeader__container__header">
                 <h1>GardOn</h1>
-                <ModeSwitch checked={scheduleMode} onChange={setScheduleMode}/>
+                <ModeSwitch isScheduleMode={isScheduleMode} setIsScheduleMode={setIsScheduleMode}/>
             </div>
         </div>
         <div>
